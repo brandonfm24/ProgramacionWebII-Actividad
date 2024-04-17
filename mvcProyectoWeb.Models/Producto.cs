87543777d5a4a8fa369fd79440dc1c9725f3cc17
brandonfm24 <brandonfm99@gmail.com>
2024-04-17 19:13:19 -0400

    PrimerActividad

---
+++ mvcProyectoWeb.Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvcProyectoWeb.Models
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public string NombreProducto { get; set; }
+        public int CantidadMinima { get; set; }
+
+    }
+}
---
+++ mvcProyectoWeb/Areas/Admin/Controllers/ProductosController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvcProyectoWeb.AccesoDatos.Data.Repository.IRepository;
+
+namespace mvcProyectoWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+
+    public class ProductosController : Controller
+    {
+        private readonly IContenedorTrabajo _context;
+        public ProductosController(IContenedorTrabajo context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult Getall()
+        {
+            return Json(new { data = _context.Producto.GetAll() });
+        }
+
+
+
+
+
+    }
+}
